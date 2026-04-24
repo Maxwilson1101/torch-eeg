21bd626a882d29aed7454b6e94684dc71356110a
Maxwilson1101 <afu-1101@sjtu.edu.cn>
2026-04-24 21:58:48 +0800

    refactor: dataloader with routing mechanics and stronger type hint

--- dataloader.py
+++ dataloader.py
@@ -1,45 +1,45 @@
 from pathlib import Path
+from typing import Literal
 
 import numpy as np
-import torch
+from numpy.typing import ArrayLike
 from torch.utils.data import Dataset
 
 
-class EEGTrainDataset(Dataset):
-    def __init__(self, file: Path):
-        d = np.load(file)
-
-        # normalize data
-        x = d["train_data"]
-        mean = x.mean(axis=0, keepdims=True)
-        std = x.std(axis=0, keepdims=True)
-        x = (x - mean) / (std + 1e-6)
-
-        self.feats = torch.from_numpy(x).float()
-        self.labels = torch.from_numpy(d["train_label"]).long()
-
-    def __len__(self):
-        return len(self.feats)
-
-    def __getitem__(self, idx):
-        return self.feats[idx], self.labels[idx]
+Split = Literal["train", "test"]
 
 
-class EEGTestDataset(Dataset):
-    def __init__(self, file: Path):
-        d = np.load(file)
+class EEGDataset(Dataset):
+    """
+    Build EEG dataset from npz file
 
-        # normalize data
-        x = d["test_data"]
-        mean = x.mean(axis=0, keepdims=True)
-        std = x.std(axis=0, keepdims=True)
-        x = (x - mean) / (std + 1e-6)
+    Warning: npz files are guaranteed in some format
+    """
 
-        self.feats = torch.from_numpy(x).float()
-        self.labels = torch.from_numpy(d["test_label"]).long()
+    KEYS = {
+        "train": ("train_data", "train_label"),
+        "test": ("test_data", "test_label"),
+    }
 
-    def __len__(self):
-        return len(self.feats)
+    def __init__(self, npz_file: Path, split: Split = "train"):
+        d = np.load(npz_file)
+        data_key, label_key = self.KEYS[split]
 
-    def __getitem__(self, idx):
-        return self.feats[idx], self.labels[idx]
+        self.data = d[data_key]
+        self.label = np.astype(d[label_key], np.int64)
+
+    def __getitem__(self, idx: int) -> tuple[ArrayLike, ArrayLike]:
+        return self.data[idx], self.label[idx]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    ROOT = Path("data")
+    ds = EEGDataset(ROOT / "1.npz")
+    pprint(ds)
+
+    pprint(ds[0])
